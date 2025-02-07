2f273d41050dbb8c0be32a489e7864b8daccf1f6
Carlos Souza <carlos.souza@pitzi.com.br>
2025-02-07 14:23:13 -0300

    esteira RunOff In-House

--- modulo/data_processor.py
+++ modulo/data_processor.py
@@ -176,7 +176,7 @@ def determinar_esteira(parceiro, origem, garantia_funcional, reincidente, runoff
         
    # 2. Verifica RUNOFF
     if (
-        #modelo in modelos_ativos and 
+        modelo in modelos_ativos and 
         status_sr in ["open", "arrived"] and  
         runoff == "runoff"  
     ):
